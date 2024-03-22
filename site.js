79026f34e9ecd8afb0ee4c44f3782d2e3d024960
124msalvani <124msalvani@frhsd.com>
2024-03-22 08:33:08 -0400

    Completed date function, likes and dislikes, working on posterClick

--- site.js
+++ site.js
@@ -45,7 +45,63 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray){
+                  let month = "";
+                  switch (dateArray[1]) {
+                        case 1:
+                           month = "January";
+                          break;
+                        case 2:
+                        month = "February";
+                          break;
+                        case 3:
+                          month = "March";
+                          break;
+                        case 4:
+                          month = "April";
+                          break;
+                        case 5:
+                          month = "May";
+                          break;
+                        case 6:
+                          month = "June";
+                          break;
+                        case 7:
+                          month = "July";
+                          break;
+                        case 9:
+                          month = "August";
+                          break;
+                        case 10:
+                          month = "September";
+                          break;
+                        case 11:
+                          month = "November";
+                          break;
+                        case 12:
+                          month = "December";
+                        break;
+                      };
+                  theRealDate = month + " " + dateArray[2] + " " + dateArray[0]
+                  return theRealDate;
+                  
+
+            },
+            
+            posterClick(){
+                  var mCurrentIndex = 0;
+                  console.log("Pain");
+                  if (mCurrentIndex >= movies.posters.length){
+                        mCurrentIndex = 0;
+                  }
+                  if(mCurrentIndex <0){
+                        mCurrentIndex = movies.posters.length -1;
+                  }
+                  mCurrentIndex++;
+            }
+            
       }
 })
 
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
+
